1e2ddbcdcbcd696414d6f636fb571b393738f949
Jamie <jmorgan3475@gmail.com>
2021-09-28 19:55:26 -0600

    state in toolbar

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,10 +1,13 @@
 import { data } from "../../SpeakerData";
-import Speaker from "./Speaker";
+import SpeakersList from "./SpeakersList";
+import Header from "./Header";
+import SpeakersToolbar from "./SpeakersToolbar";
 
 const Speakers = () => {
   return (
     <div className="container-fluid">
       <Header />
+      <SpeakersToolbar />
       <SpeakersList data={data} />
     </div>
   );
